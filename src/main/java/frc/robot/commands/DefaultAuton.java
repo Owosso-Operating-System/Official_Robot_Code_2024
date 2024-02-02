4aa6aa701057a801e73952832ac95e85da71e6c7
Owosso-Operating-System <5260OwossoOperatingSystem@gmail.com>
2024-02-01 19:43:33 -0500

    Made auton better

--- src/main/java/frc/robot/commands/DefaultAuton.java
+++ src/main/java/frc/robot/commands/DefaultAuton.java
@@ -6,14 +6,18 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.*;
 
 public class DefaultAuton extends Command {
   /** Creates a new DefaultAuton. */
   //Makes a variable named driveTrain
   private DriveTrain driveTrain;
-  public DefaultAuton(DriveTrain driveTrain) {
+  private ScoringSubsystem scoringSubsystem;
+  private HandoffSubsystem handoffSubsystem;
+  public DefaultAuton(DriveTrain driveTrain, ScoringSubsystem scoringSubsystem, HandoffSubsystem handoffSubsystem) {
     this.driveTrain = driveTrain;
+    this.scoringSubsystem = scoringSubsystem;
+    this.handoffSubsystem = handoffSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
   }
@@ -21,6 +25,7 @@ public class DefaultAuton extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    /* 
     //Set the driveTrain speed to -0.2
     driveTrain.mecDrive.driveCartesian(-.2, 0, 0);
     //Waits for 5 seconds
@@ -29,6 +34,35 @@ public class DefaultAuton extends Command {
     driveTrain.mecDrive.driveCartesian(0, 0, 0);
     //Runs isFinished
     isFinished();
+    */
+    driveTrain.mecDrive.driveCartesian(0, -.5, 0);
+    Timer.delay(2);
+    driveTrain.mecDrive.driveCartesian(0, 0, 0);
+    scoringSubsystem.scorer.set(1);
+    Timer.delay(.1);
+    scoringSubsystem.scorer.set(0);
+    driveTrain.mecDrive.driveCartesian(-.1, 0, 0);
+    Timer.delay(.2);
+    driveTrain.mecDrive.driveCartesian(0, 0, -.5);
+    Timer.delay(.3);
+    driveTrain.mecDrive.driveCartesian(0, 0, 0);
+    handoffSubsystem.handoff.set(.5);
+    Timer.delay(.3);
+    scoringSubsystem.scorer.set(-1);
+    Timer.delay(.1);
+    scoringSubsystem.scorer.set(0);
+    driveTrain.mecDrive.driveCartesian(0, 0, .5);
+    Timer.delay(.3);
+    driveTrain.mecDrive.driveCartesian(.1, 0, 0);
+    Timer.delay(.2);
+    driveTrain.mecDrive.driveCartesian(0, 0, 0);
+    scoringSubsystem.scorer.set(1);
+    Timer.delay(.1);
+    scoringSubsystem.scorer.set(0);
+    driveTrain.mecDrive.driveCartesian(0, -.5, 0);
+    Timer.delay(2.784);
+    isFinished();
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
