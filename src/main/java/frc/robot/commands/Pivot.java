c8ca989f262ae556ecd9cc9aeb39a01dbedd442b
Owosso-Operating-System <5260OwossoOperatingSystem@gmail.com>
2024-02-03 10:57:17 -0500

    Changed HandOff to Pivot

--- src/main/java/frc/robot/commands/Pivot.java
+++ src/main/java/frc/robot/commands/Pivot.java
@@ -6,43 +6,46 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.HandoffSubsystem;
+import frc.robot.subsystems.PivotSubsystem;
 
-public class Handoff extends Command {
-  /** Creates a new HandOff. */
-  // Creates new HandOff Object named handoff
- public final HandoffSubsystem handoffSubsystem;
+public class Pivot extends Command {
+  /** Creates a new Pivot. */
+  // Creates new Pivot Object named Pivot
+ public final PivotSubsystem pivotSubsystem;
 
   // Creates new XboxController Object named controller
   public final XboxController controller;
 
-    /**Method: HandOff
-   * Parameters: handOffSubsystem and XboxController
-   * Variables used: handOffSubsystem and controller
-   * What it does: Assigns the parameter handOffSubsystem to HandOffsubsystem
+    /**Method: Pivot
+   * Parameters: PivotSubsystem and XboxController
+   * Variables used: PivotSubsystem and controller
+   * What it does: Assigns the parameter PivotSubsystem to Pivotsubsystem
    *               Assigns the parameter XboxController to controller
-   *               Uses addRequirements to tie handOffSubsystem to handOff
+   *               Uses addRequirements to tie PivotSubsystem to Pivot
    *  */
 
-  public Handoff(HandoffSubsystem handoffSubsystem, XboxController controller) {
-    this.handoffSubsystem = handoffSubsystem;
+  public Pivot(PivotSubsystem pivotSubsystem, XboxController controller) {
+    this.pivotSubsystem = pivotSubsystem;
     this.controller = controller;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(handoffSubsystem);
+    addRequirements(pivotSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-    /**Method:Handoff
+    /**Method:Pivot
    * Parameters: None
-   * Variables used: handoffSubsystems and controller
-   * What it does: Takes the controller outputs, passes the values to handoff
+   * Variables used: PivotSubsystems and controller
+   * What it does: Takes the controller outputs, passes the values to Pivot
    *  */
   @Override
   public void execute() {
     //IDK, fill in later
+    if(controller.getRawAxis(5) > 0.2 || controller.getRawAxis(5) < -0.2){
+      pivotSubsystem.pivot.set(controller.getRawAxis(5)*.25);
+    }
   }
 
   // Called once the command ends or is interrupted.
